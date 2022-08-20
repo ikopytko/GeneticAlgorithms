ded4da6d2b1dfaebf9080107db6b2276d945a776
Ivan <handlemind@gmail.com>
2022-08-20 17:02:34 +0200

    Extract map parser

---
+++ Maze/MapParser.cs
@@ -0,0 +1,33 @@
+﻿namespace Maze;
+
+public interface IMapParser
+{
+    int[,] GetMap();
+}
+
+public class StringMapParser : IMapParser
+{
+    private readonly string _source;
+
+    public StringMapParser(string source)
+    {
+        _source = source;
+    }
+    public int[,] GetMap()
+    {
+        var lines = _source.Split(Environment.NewLine);
+        var map = new int[lines.Length, lines[0].Length];
+        for (int row = 0; row < lines.Length; row++)
+        {
+            var line = lines[row];
+            if (line.Length != map.GetLength(1))
+                throw new ArgumentException(line);
+            for (int col = 0; col < line.Length; col++)
+            {
+                map[row, col] = int.Parse(line.AsSpan().Slice(col, 1));
+            }
+        }
+
+        return map;
+    }
+}
--- Maze/Program.cs
+++ Maze/Program.cs
@@ -1,11 +1,26 @@
 ﻿
+using Maze;
+
 const double crossoverRate = 0.7;
 const double mutationRate = 0.001;
 const int populationSize = 140;
 const int chromosomeLength = 70;
 const int geneLength = 2;
 
-var map = new Map();
+string mapData =
+    @"111111111111111
+101000001110001
+300000001110001
+100011100100001
+100011100000101
+110011100000101
+100001000011101
+101100010000002
+101100010000001
+111111111111111";
+
+IMapParser parser = new StringMapParser(mapData);
+var map = new Map(parser.GetMap());
 
 var agent = new CgaAgent(map, crossoverRate, mutationRate, populationSize, chromosomeLength, geneLength);
 
